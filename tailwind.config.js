328c0b32c5dc427c74ca8305c2d07bded4a9070d
FazliddinNizomiddinovf <nizomiddinov20112012@gmail.com>
2026-02-17 19:32:06 +0500

    Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '920px',
+          xl: '1184px'
+        }
+      },
+    },
+    screens: {
+      xxss: "400px"
+    },
   },
   plugins: [],
 }
