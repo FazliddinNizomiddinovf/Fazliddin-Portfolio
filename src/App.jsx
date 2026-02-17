a8ca7ec98d09aa7524f8ee30b55eee6e6fa723c6
FazliddinNizomiddinovf <nizomiddinov20112012@gmail.com>
2026-02-17 19:44:12 +0500

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     },
   ])
   return (
-    <div className='container'>
+    <div className='containers'>
       <IntroLoader/>
       <RouterProvider router={routes} />
     </div>
